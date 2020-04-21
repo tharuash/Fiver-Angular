19dd4dd54ca5a28d35369d6d897b9d3dcd6060bc
tharuash <tharinduash@outlook.com>
2020-04-21 20:49:36 +0530

    ngFor added

--- src/app/components/popular/popular.component.ts
+++ src/app/components/popular/popular.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./popular.component.css']
 })
 export class PopularComponent implements OnInit {
-
+  imageDetails = [
+    {path : '../../../assets/img/popular1.jpeg', left : '200px', name: 'Lamborghini', price: '450'},
+    {path : '../../../assets/img/popular2.jpg', left : '650px', name: 'Jaguar', price: '350'},
+    {path : '../../../assets/img/popular3.jpeg', left : '1050px', name: 'Camaro', price: '250'},
+  ];
   constructor() { }
 
   ngOnInit() {
